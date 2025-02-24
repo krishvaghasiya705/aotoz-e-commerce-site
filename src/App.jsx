55d7deae61a51134fc05f40d25f90c390f89f903
krishvaghasiya705 <krishvaghasiya705@gmail.com>
2025-02-24 15:18:59 +0530

    removed ai assistent for chat

--- src/App.jsx
+++ src/App.jsx
@@ -10,17 +10,17 @@ import Liked from "./pages/like";
 import Homecardsdetails from "./pages/homecardsdetails";
 
 const App = () => {
-  useEffect(() => {
-    var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
-    (function(){
-      var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
-      s1.async = true;
-      s1.src = 'https://embed.tawk.to/67b4212d167f1f1900573b77/1ikcffqg1';
-      s1.charset = 'UTF-8';
-      s1.setAttribute('crossorigin', '*');
-      s0.parentNode.insertBefore(s1, s0);
-    })();
-  }, []);
+  // useEffect(() => {
+  //   var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+  //   (function(){
+  //     var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+  //     s1.async = true;
+  //     s1.src = 'https://embed.tawk.to/67b4212d167f1f1900573b77/1ikcffqg1';
+  //     s1.charset = 'UTF-8';
+  //     s1.setAttribute('crossorigin', '*');
+  //     s0.parentNode.insertBefore(s1, s0);
+  //   })();
+  // }, []);
 
   return (
     <div className="font-jaro">
